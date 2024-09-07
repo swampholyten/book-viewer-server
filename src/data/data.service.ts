c0f303dc98f9101d5ef7fa1c7bc09b4ee4f8e912
Ji Junkai <junkai.ji@studio.unibo.it>
2024-09-08 00:26:41 +0200

    create server

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 @Injectable()
 export class DataService {
   async getData(): Promise<any> {
-    const filePath = path.join(__dirname, '..', '..', 'data10.json');
+    const filePath = path.join(__dirname, '..', '..', 'data.json');
     return new Promise((resolve, reject) => {
       fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
